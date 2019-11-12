b9cb3059333015c6842becc6010fc7222689c63e
adiaux <aditya8anil@gmail.com>
2019-11-12 19:54:13 +0530

    Run for numerous shapes

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from shapely.geometry import Polygon
 
 def polygonArea(X, Y, n): 
     area = 0.0
@@ -11,29 +12,53 @@ def polygonArea(X, Y, n):
   
     return int(abs(area / 2.0)) 
 
-X=[]
-X1=[]
-Y=[]
-Y1=[]
-sides =int(input("Enter the number of sides :"))
-for vertice in range(1,(sides+1)):
-  X.append(int((input("Enter the x co-ordinate of the side " +str(vertice)+" : "))))
-  Y.append(int((input("Enter the y co-ordinate of the side " +str(vertice)+" : "))))
-
-n = int(len(X)) 
-print(polygonArea(X, Y, n)) 
-
-#Making the image
+def combineXY(X,Y):
+  bothXY=[]
+  for i in range(len(X)):
+    temp=[]
+    for j in range(1):
+        temp.append(X[i])
+        temp.append(Y[i])
+        bothXY.append(temp)
+  return bothXY
+"""
+def centroidTotal(areaList, centroidList):
+  shapeCentroid=[]
+  areaSum=0
+  for i in range(len(areaList)):
+    areaSum += areaList[i]
+    sigmaX=areaList[i]*centroidList[i][0]
+    sigmaY=areaList[i]*centroidList[i][1]
+  finalX=sigmaX/areaSum
+  finalY=sigmaY/areaSum
+  shapeCentroid.append(finalX)
+  shapeCentroid.append(finalY)
+  return shapeCentroid
+"""
+areaList=[]
+centroidList=[]
 img = np.zeros([512,512,1],dtype=np.uint8)
 img.fill(255)
-for x,y in range(0,n):
-  cv2.line(img,(X[x],Y[y]),(X[x+1],Y[y+1]),(0,0,0),5)
-cv2.imshow('Single Channel Window', img)
+cv2.line(img,(256,0),(256,512),(0,0,0),1)
+cv2.line(img,(0,256),(512,256),(0,0,0),1)
+for choice in range(int(input("Enter a number OF shapes : "))):
+  X=[]
+  Y=[]
+  for vertice in range(1,(int(input("Enter the number of sides :"))+1)):
+    print("Note the maximum lenth is 256 units")
+    X.append(int((input("Enter the x co-ordinate of the side " +str(vertice)+" : "))))
+    Y.append(int((input("Enter the y co-ordinate of the side " +str(vertice)+" : ")))) 
+  for x in range(len(X)-1):
+    cv2.line(img,(X[x]+256,256-Y[x]),(X[x+1]+256,256-Y[x+1]),(0,0,0),2)
+  cv2.line(img,(X[0]+256,256-Y[0]),(X[len(X)-1]+256,256-Y[len(X)-1]),(0,0,0),2)
+  areaList.append(polygonArea(X, Y, len(X)))
+  centroidList.append(list(Polygon(combineXY(X,Y)).centroid.coords))
 
-print("image shape: ", img.shape)
- 
+print("The centroids are "+str(centroidList))
+print("The areas are "+str(areaList))
+#print(centroidTotal(areaList,centroidList))
+cv2.imshow('Single Channel Window', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 
 
--- shape.py
+++ shape.py
@@ -1,24 +1,11 @@
-"""
-import numpy as np
-import cv2
+X=[0,100,100,0]
+Y=[0,0,100,100]
 
-# Create a black image
-img = np.zeros([512,512,1],dtype=np.uint8)
-
-img = cv2.line(img,(0,0),(511,511),(255,0,0),5)
-cv2.imwrite('test.png',img)
-"""
-
-import cv2
-import numpy as np
-img = np.zeros([512,512,1],dtype=np.uint8)
-img.fill(255)
-X=[0,100,5,0]
-Y=[0,100,5,5]
-cv2.line(img,(X[0],Y[0]),(X[1],Y[1]),(0,0,0),5)
-cv2.imshow('Single Channel Window', img)
-
-print("image shape: ", img.shape)
- 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+main=[]
+for i in range(len(X)):
+    temp=[]
+    for j in range(1):
+        temp.append(X[i])
+        temp.append(Y[i])
+        main.append(temp)
+print(main)
