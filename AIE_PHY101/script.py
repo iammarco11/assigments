00256e00ed10a32032654aacbd7a11e86d2a2ae5
adiaux <aditya8anil@gmail.com>
2019-11-19 17:15:22 +0530

    Working code

--- AIE_PHY101/script.py
+++ AIE_PHY101/script.py
@@ -44,15 +44,15 @@ img = np.zeros([512,512,1],dtype=np.uint8)
 img.fill(255)
 cv2.line(img,(256,0),(256,512),(0,0,0),1)
 cv2.line(img,(0,256),(512,256),(0,0,0),1)
-n=int(input("Enter a number OF shapes :")) 
-for choice in range(n):
+for choice in range(int(input("Enter a number OF shapes : "))):
   X=[]
   Y=[]
-  Cx=0.0
-  Cy=0.0
-  Ix=0.0
-  Iy=0.0
-  for vertice in range(1,(int(input("Enter the number of sides :"))+1)):
+  Cx=0
+  Cy=0
+  Ix=0
+  Iy=0
+  n = int(input("Enter the number of sides :"))
+  for vertice in range(1,n+1):
     print("Note the maximum lenth is 256 units")
     X.append(int((input("Enter the x co-ordinate of the side " +str(vertice)+" : "))))
     Y.append(int((input("Enter the y co-ordinate of the side " +str(vertice)+" : ")))) 
@@ -62,37 +62,39 @@ for choice in range(n):
   areaList.append(polygonArea(X, Y, len(X)))
   centroidList.append(list(Polygon(combineXY(X,Y)).centroid.coords))
 
+for i in range(0,n):
+  k=i+1
+  if k>=n:
+      k=0
+  Cx+=(X[i]+X[k])*(X[i]*Y[k]-X[k]*Y[i])
+  Cy+=(Y[i]+Y[k])*(X[i]*Y[k]-X[k]*Y[i])
+
+  Cx=1/(6*areaList[choice])*Cx
+  Cy=1/(6*areaList[choice])*Cy
+  for i in range(len(X)):
+      X[i]=X[i]+Cx
+  for i in range(len(Y)):
+      Y[i]=Y[i]+Cx
   for i in range(0,n):
-    k=i+1
-    if k>=n:
-        k=0
-    Cx+=(X[i]+X[k])*(X[i]*Y[k]-X[k]*Y[i])
-    Cy+=(Y[i]+Y[k])*(X[i]*Y[k]-X[k]*Y[i])
-        
-    Cx=1/(6*areaList[choice])*Cx
-    Cy=1/(6*areaList[choice])*Cy
-    for i in range(len(X)):
-        X[i]=X[i]+Cx
-    for i in range(len(Y)):
-        Y[i]=Y[i]+Cx
-    for i in range(0,n):
-        k=i+1
-        if k>=n:
-            k=0
-        Ix+=(X[i]*Y[k]-X[k]*Y[i])*(Y[i]*Y[i]+Y[i]*Y[k]+Y[k]*Y[k])
-        Iy+=(X[i]*Y[k]-X[k]*Y[i])*(X[i]*X[i]+X[i]*X[k]+X[k]*X[k])
-    Ix=(1/12)*Ix
-    Iy=(1/12)*Iy
-    tempList1=[]
-    tempList1.append(Cx)
-    tempList1.append(Cy)
-    centroidList.append(tempList1)
-    tempList2=[]
-    tempList2.append(Ix)
-    tempList2.append(Iy)
-    inertiaList.append(tempList2)
+      k=i+1
+      if k>=n:
+          k=0
+      Ix+=(X[i]*Y[k]-X[k]*Y[i])*(Y[i]*Y[i]+Y[i]*Y[k]+Y[k]*Y[k])
+      Iy+=(X[i]*Y[k]-X[k]*Y[i])*(X[i]*X[i]+X[i]*X[k]+X[k]*X[k])
+  Ix=(1/12)*Ix
+  Iy=(1/12)*Iy
+  ListCX=[]
+  ListCY=[]
 
+  ListCX.append(Cx)
+  ListCY.append(Cy)
+  ListIx=[]
+  ListIy=[]
+  ListIx.append(Ix)
+  ListIy.append(Iy)
 
+print("The moment of inertia X axis"+str(ListIx))
+print("The moment of inertia Y axis"+str(ListIy))
 print("The centroids are "+str(centroidList))
 print("The areas are "+str(areaList))
 print("The centroid of the whole shape is "+str(centroidTotal(areaList,centroidList)))
